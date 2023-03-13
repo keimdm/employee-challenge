d5bdd21d1bd865d59ad3ba4fe1ee2f52c6430309
David Keim <dmk252@cornell.edu>
2023-03-13 16:45:26 -0400

    added initial server and db functionality

--- server.js
+++ server.js
@@ -1,13 +1,37 @@
 // DEPENDENCIES
+const express = require("express");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
 // DATA
 
 // DB CONNECTION
-
-// MIDDLEWARE
+mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "dddddddd",
+    database: "employee_data_db"
+});
 
 // APP/PORT NUMBER
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// MIDDLEWARE
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 // ROUTES
 
+app.get('*', (req, res) => {
+    res.send("Hello World");
+});
+
+app.use((req, res) => {
+    res.status(404).end();
+  });
+
 // LISTENER
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+});
