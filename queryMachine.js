2dc59e833e696fb412d721d87714b10ee48c98fb
David Keim <dmk252@cornell.edu>
2023-03-14 17:28:05 -0400

    updated options to show names/titles instead of ids, used promise on all queries

--- queryMachine.js
+++ queryMachine.js
@@ -1,16 +1,47 @@
 class queryMachine {
     constructor() {}
 
-    selectDepartmentQuery() {
-        return 'SELECT * FROM department;';
+    selectDepartmentQuery(x) {
+        if (x) {
+            return `SELECT ${x} FROM department;`;
+        }
+        else {
+            return `SELECT * FROM department;`;
+        } 
     }
 
-    selectRoleQuery() {
-        return 'SELECT * FROM role;';
+    lookupDepartmentQuery(x) {
+        return `SELECT id FROM department WHERE name = "${x}";`
     }
 
-    selectEmployeeQuery() {
-        return 'SELECT * FROM employee;';
+    selectRoleQuery(x) {
+        if (x) {
+            return `SELECT ${x} FROM role;`;
+        }
+        else {
+            return `SELECT * FROM role;`;
+        } 
+    }
+
+    lookupRoleQuery(x) {
+        return `SELECT id FROM role WHERE title = "${x}";`
+    }
+
+    selectEmployeeQuery(x) {
+        if (x) {
+            return `SELECT ${x} FROM employee;`;
+        }
+        else {
+            return `SELECT * FROM employee;`;
+        } 
+    }
+
+    selectEmployeeName() {
+        return `SELECT first_name, last_name FROM employee`;
+    }
+
+    lookupEmployeeName(x, y) {
+        return `SELECT id FROM employee WHERE first_name = "${x}" AND last_name = "${y}"`;
     }
 
     addDepartmentQuery(newDeptName)  {
--- server.js
+++ server.js
@@ -27,7 +27,7 @@ const addDepartment = [
 ];
 
 // additional prompts for add role option
-const addRole = [
+let addRole = [
     {
         type: 'input',
         message: 'Please enter the name of the new role:',
@@ -39,14 +39,15 @@ const addRole = [
         name: 'salary',
     },
     {
-        type: 'input',
+        type: 'list',
         message: 'Please enter the department ID of the new role:',
+        choices: [],
         name: 'deptID',
     }
 ];
 
 // additional prompts for add employee option
-const addEmployee = [
+let addEmployee = [
     {
         type: 'input',
         message: 'Please enter the first name of the new employee:',
@@ -58,27 +59,31 @@ const addEmployee = [
         name: 'empLastName',
     },
     {
-        type: 'input',
-        message: "Please enter the new employee's role ID:",
+        type: 'list',
+        message: "Please select the new employee's role:",
+        choices: [],
         name: 'empRoleID',
     },
     {
-        type: 'input',
-        message: "Please enter the new employee's manager ID:",
+        type: 'list',
+        message: "Please select the new employee's manager:",
+        choices: [],
         name: 'empMgrID',
     }
 ];
 
 // additional prompts for update employee role option
-const updateEmployeeRole = [
+let updateEmployeeRole = [
     {
-        type: 'input',
-        message: "Please enter the ID of the employee whose role is changing:",
+        type: 'list',
+        message: "Please select the employee whose role is changing:",
+        choices: [],
         name: 'empIDUpdate',
     },
     {
-        type: 'input',
-        message: "Please enter the selected employee's new role ID:",
+        type: 'list',
+        message: "Please select the employee's new role:",
+        choices: [],
         name: 'empRoleUpdate',
     },
 ];
@@ -180,79 +185,134 @@ function runMenu() {
                 });
                 break;
             case "Add A Role":
+                let newRoleName;
+                let newSalary;
                 // additional inquirer prompts to gather inputs
-                inquirer.prompt(addRole).then((response) => {
-                    let newRoleName = response.roleName;
-                    let newSalary = Number(response.salary);
-                    let newDeptID = Number(response.deptID);
-                    if (newRoleName && newSalary && newDeptID) {
-                        // query to insert new role
-                        db.query(queryMachine.addRoleQuery(newRoleName, newSalary, newDeptID), function (err, results) {
-                            if (err) {
-                                console.error(err);
-                                runMenu();
-                            }
-                            else {
-                                console.log(`Successfully added ${newRoleName} as a role`);
-                                runMenu();
-                            }
-                        });
-                    }
-                    else {
-                        console.log("Invalid input - please try again!");
+                db.promise().query(queryMachine.selectDepartmentQuery("name"))
+                    .then((response) => {
+                        for (i = 0; i < response[0].length; i++) {
+                            addRole[2].choices.push(response[0][i].name);
+                        }
+                        return inquirer.prompt(addRole);
+                    })
+                    .then((response) => {
+                        addRole[2].choices = [];
+                        newRoleName = response.roleName;
+                        newSalary = Number(response.salary);
+                        return db.promise().query(queryMachine.lookupDepartmentQuery(response.deptID));
+                    })
+                    .then((response) => {
+                        if (newRoleName && newSalary && response[0][0].id) {
+                            return db.promise().query(queryMachine.addRoleQuery(newRoleName, newSalary, response[0][0].id));
+                        }
+                        else {
+                            return new Error("Invalid input - please try again!");
+                        }
+                    })
+                    .then((response) => {
+                        if (response instanceof Error) {
+                            console.log(response);
+                        }
+                        else {
+                            console.log(`Successfully added ${newRoleName} as a role`);
+                        }
                         runMenu();
-                    }
-                });
+                    });
                 break;
             case "Add An Employee":
-                // additional prompt to gather info
-                inquirer.prompt(addEmployee).then((response) => {
-                    let newEmpFirst = response.empFirstName;
-                    let newEmpLast = response.empLastName;
-                    let newEmpRole = Number(response.empRoleID);
-                    let newEmpMgr = Number(response.empMgrID);
-                    if (newEmpFirst && newEmpLast && newEmpRole && newEmpMgr) {
-                        // query to insert new employee
-                        db.query(queryMachine.addEmployeeQuery(newEmpFirst, newEmpLast, newEmpRole, newEmpMgr), function (err, results) {
-                            if (err) {
-                                console.error(err);
-                                runMenu();
-                            }
-                            else {
-                                console.log(`Successfully added ${newEmpFirst} ${newEmpLast} as an employee`);
-                                runMenu();
-                            }
-                        });
-                    }
-                    else {
-                        console.log("Invalid input - please try again!");
+                let newEmpFirst;
+                let newEmpLast;
+                let newEmpRole;
+                let newEmpMgr;
+                db.promise().query(queryMachine.selectRoleQuery("title"))
+                    .then((response) => {
+                        for (i = 0; i < response[0].length; i++) {
+                            addEmployee[2].choices.push(response[0][i].title);
+                        }
+                        return db.promise().query(queryMachine.selectEmployeeName())
+                    })
+                    .then((response) => {
+                        for (i = 0; i < response[0].length; i++) {
+                            addEmployee[3].choices.push(response[0][i].first_name + " " + response[0][i].last_name);
+                        }
+                        return inquirer.prompt(addEmployee);
+                    })
+                    .then((response) => {
+                        addEmployee[2].choices = [];
+                        addEmployee[3].choices = [];
+                        newEmpFirst = response.empFirstName;
+                        newEmpLast = response.empLastName;
+                        newEmpMgr = response.empMgrID;
+                        return db.promise().query(queryMachine.lookupRoleQuery(response.empRoleID));
+                    })
+                    .then((response) => {
+                        newEmpRole = response[0][0].id;
+                        let empMgrLookup = newEmpMgr.split(" ");
+                        return db.promise().query(queryMachine.lookupEmployeeName(empMgrLookup[0], empMgrLookup[1]))
+                    })
+                    .then((response) => {
+                        if (newEmpFirst && newEmpLast && newEmpRole && response[0][0].id) {
+                            return db.promise().query(queryMachine.addEmployeeQuery(newEmpFirst, newEmpLast, newEmpRole, response[0][0].id));
+                        }
+                        else {
+                            return new Error("Invalid input - please try again!");
+                        }
+                    })
+                    .then((response) => {
+                        if (response instanceof Error) {
+                            console.log(response);
+                        }
+                        else {
+                            console.log(`Successfully added ${newEmpFirst} ${newEmpLast} as an employee`);
+                        }
                         runMenu();
-                    }
-                });
+                    });
                 break;
             case "Update An Employee Role":
+                let newIDUpdate;
+                let newRoleUpdate;
                 // additional inquirer prompt to gather inputs
-                inquirer.prompt(updateEmployeeRole).then((response) => {
-                    let newIDUpdate = Number(response.empIDUpdate);
-                    let newRoleUpdate = Number(response.empRoleUpdate);
-                    if (newIDUpdate && newRoleUpdate) {
-                        // query to update employee info
-                        db.query(queryMachine.updateEmployeeRoleQuery(newRoleUpdate, newIDUpdate), function (err, results) {
-                            if (err) {
-                                console.error(err);
-                                runMenu();
-                            }
-                            else {
-                                console.log(`Successfully updated record for employee ID ${newIDUpdate}`);
-                                runMenu();
-                            }
-                        });
-                    }
-                    else {
-                        console.log("Invalid input - please try again!");
+                db.promise().query(queryMachine.selectEmployeeName())
+                    .then((response) => {
+                        for (i = 0; i < response[0].length; i++) {
+                            updateEmployeeRole[0].choices.push(response[0][i].first_name + " " + response[0][i].last_name);
+                        }
+                        return db.promise().query(queryMachine.selectRoleQuery("title"));
+                    })
+                    .then((response) => {
+                        for (i = 0; i < response[0].length; i++) {
+                            updateEmployeeRole[1].choices.push(response[0][i].title);
+                        }
+                        return inquirer.prompt(updateEmployeeRole);
+                    })
+                    .then((response) => {
+                        updateEmployeeRole[0].choices = [];
+                        updateEmployeeRole[1].choices = [];
+                        newRoleUpdate = response.empRoleUpdate;
+                        let empNameLookup = response.empIDUpdate.split(" ");
+                        return db.promise().query(queryMachine.lookupEmployeeName(empNameLookup[0], empNameLookup[1]));
+                    })
+                    .then((response) => {
+                        newIDUpdate = response[0][0].id;
+                        return db.promise().query(queryMachine.lookupRoleQuery(newRoleUpdate));
+                    })
+                    .then((response) => {
+                        if (newIDUpdate && response[0][0].id) {
+                            return db.promise().query(queryMachine.updateEmployeeRoleQuery(response[0][0].id, newIDUpdate));
+                        }
+                        else {
+                            return new Error("Invalid input - please try again!");
+                        }
+                    })
+                    .then((response) => {
+                        if (response instanceof Error) {
+                            console.log(response);
+                        }
+                        else {
+                            console.log(`Successfully updated`);
+                        }
                         runMenu();
-                    }
-                });
+                    });
                 break;
         }
     });
